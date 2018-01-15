aa3e06bb9295105cdf91c46beb85cec263b60e79
mosh <moshahmed@gmail.com>
2018-01-15 19:21:13 +0530

    renaming files to lower-case

---
+++ layout.api-handler.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Code written by mo.ahmed@hmi-tech.net
+ *
+ * HMI Technologies Mumbai 
+ *
+ * API Service: Client end point. 
+ * 
+ */
+
+// Add your code to handle any API requests ... 
+
+ ?>
---
+++ layout.full-width.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * ezCMS Code written by mo.ahmed@hmi-tech.net and mosh.ahmed@gmail.com
+ *
+ * HMI Technologies Mumbai
+ *
+ * The layout is an example to show full width pages 
+ * without the aside content. 
+ * Change the layout of any page to this for full width content.
+ */
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	
+    <title><?php echo $page["title"]; ?></title>
+    <link rel="canonical" href="http://<?php echo $page["canonical"]; ?>">
+	
+    <meta name="keywords" content="<?php echo $page["keywords"]; ?>">
+    <meta name="description" content="<?php echo $page["description"]; ?>">
+	
+    <link rel="stylesheet" href="<?php echo $siteFolder; ?>/style.css">
+    <script src="<?php echo $siteFolder; ?>/main.js"></script>
+	
+    <?php echo $page["head"]; ?>
+
+</head>
+<body>
+
+	<header><?php echo $header; ?></header>
+	
+	<table>
+		<tr>
+			<td><main><?php echo $maincontent;?></main></td>
+		</tr>
+	</table>
+	
+	<footer><?php echo $footer; ?></footer>
+    
+</body>
+</html>
