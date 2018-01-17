4d0eda0e529ffdc10a99d87a2288a5f95c2654f7
mosh <moshahmed@gmail.com>
2018-01-17 14:44:50 +0530

    allow 1char pass

--- login/class/profile.class.php
+++ login/class/profile.class.php
@@ -40,19 +40,19 @@ class ezProfile extends ezCMS {
 		
 		// check password match
 		if ($newpass != $reppass) {
-			$this->setMsgHTML('error','Password Mismatch !',
+			$this->setMsgHTML('error','Password Mismatch!',
 				'The new password and repeat password do not match.');
 			return;
 		} 
 		
 		// check password len
-		if (strlen($newpass)<8) {
-			$this->setMsgHTML('error','Password Too Short !',
-				'The new password must be more than 8 characters in lenght.');
+		if (strlen($newpass)<1) {
+			$this->setMsgHTML('error','Password Too Short!',
+				'The new password must be more than 8 characters in length.');
 			return;
 		}
 		
-		// Prepare SQL to fetch user's record from dataabse
+		// Prepare SQL to fetch user's record from database
 		$id = $this->usr['id'];
 		$stmt = $this->prepare("SELECT `id` FROM `users` WHERE `id` = $id AND (`passwd` = SHA2( ? , 512 ) or `passwd` = ?) LIMIT 1");
 		$stmt->execute( array($curpass, $curpass) );
@@ -64,18 +64,17 @@ class ezProfile extends ezCMS {
 			$stmt = $this->prepare("UPDATE `users` SET `passwd` = SHA2( ? , 512 ) WHERE `id` = $id ");
 			if ($stmt->execute( array($newpass) ) ) {
 				// Database update done
-				$this->setMsgHTML('success','New Password Saved !',
+				$this->setMsgHTML('success','New Password Saved!',
 					'You have successfully changed your password.');
 			} else {
 				// Database update failed
-				$this->setMsgHTML('error','Update Failed !',
+				$this->setMsgHTML('error','Update Failed!',
 					'Failed to update your password.');
 			}
 
 		} else {
-		
 				// Database update failed
-				$this->setMsgHTML('error','Current Password Mismatch !',
+				$this->setMsgHTML('error','Current Password Mismatch!',
 					'Your current password is incorrect.');
 		}
 	}
