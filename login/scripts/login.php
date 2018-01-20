1dcfd6e0f63a4cc0cf893d67ec91d9cc525ec242
mohdahmed <mo.ahmed@hmi-tech.net>
2018-01-20 15:16:25 +0530

    Added feature to hash password on first login in case it is not.

--- login/scripts/login.php
+++ login/scripts/login.php
@@ -61,9 +61,13 @@ if ($stmt->rowCount()) {
 	$_SESSION['EZUSERID']   = $user['id']; // User's ID
 	
 	// TODO - Move to database later ...
-	
 	$_SESSION['EDITORTYPE'] = $user['editor']; // Set editor to code mirror on login
 	$_SESSION["CMTHEME"]    = $user['cmtheme']; // Default Code Mirror theme
+	
+	// Encrypt the password on first login if is not.
+	if ($user['passwd'] == $passwd)
+		// $passwd is not hashed 
+		$dbh->query('UPDATE `users` SET `passwd` = SHA2( `passwd` , 512 ) WHERE `id`='.$user['id']);
 	
 	// Redirect to logged in page
 	if ( isset( $_SESSION['AFTERLOGINPAGE'] )) {
