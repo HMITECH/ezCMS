36721ff216c33f022a1a7a3307726beae8c63b00
Avinash <avinash@hmi-tech.net>
2018-01-22 13:52:12 +0530

    Updated canonical url

--- layout.php
+++ layout.php
@@ -18,7 +18,7 @@
 	<meta charset="utf-8">
 
     <title><?php echo $page["title"]; ?></title>
-    <link rel="canonical" href="http://<?php echo $page["canonical"]; ?>">
+    <link rel="canonical" href="<?php echo $page["canonical"]; ?>">
 
     <meta name="keywords" content="<?php echo $page["keywords"]; ?>">
     <meta name="description" content="<?php echo $page["description"]; ?>">
