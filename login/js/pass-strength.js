6f23a1025c147504debf570efef0cdd72ced5c29
mosh <moshahmed@gmail.com>
2018-01-22 22:48:06 +0530

    allow 5 char password as green, and opacity of color

--- login/js/pass-strength.js
+++ login/js/pass-strength.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 	$('input[type=password]').keyup(function() {
 		var password = $(this).val();
+    var quality = 3.0; // password quality: 6=high ..  2=low
 
     var strength   = 0;
-    strength += password.length / 6;
+    strength += password.length / quality;
     if ( password.match(/[a-z]/) )      strength++;
     if ( password.match(/[A-Z]/) )      strength++;
     if ( password.match(/\d+/))         strength++;
     if ( password.match(/[^a-z\d]+/) )	strength++;
 
-    strength = strength/6.0;
+    strength = strength/quality;
     strength = Math.max(0, strength);
     strength = Math.min(1, strength);
 
@@ -18,13 +19,13 @@ $(document).ready(function() {
     var g = Math.floor(255*(strength));
     var b = Math.floor(255*(1-strength)*strength*4);
 
-    var color = 'rgb( '+r+' , '+g+' , '+b+' )';
+    var color = 'rgba( '+r+' , '+g+' , '+b+', .4 )';
     $(this).css('background-color', color);
 
     var myid = $(this).attr("id");
     if (myid == 'password2') {
-        var password1 = $("#user_pass").val();
-        var password2 = $("#user_pass1").val();
+        var password1 = $("#password1").val();
+        var password2 = $("#password2").val();
         if(password1 == password2) {
             $("#validate-status").text("equal");
         } else {
