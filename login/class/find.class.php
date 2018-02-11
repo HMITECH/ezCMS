8cf917d421ae93038999eebc41e9976e5821d78f
mohdahmed <mo.ahmed@hmi-tech.net>
2018-02-12 00:31:02 +0530

    Working on find and replace ... final UI

--- login/class/find.class.php
+++ login/class/find.class.php
@@ -18,8 +18,83 @@ class ezFind extends ezCMS {
 	
 		// call parent constuctor
 		parent::__construct();
+		
+		// Handle post request
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$r = new stdClass();
+			$r->success = true;
+			$findin = $_POST['findinTxt'];
+			if ($findin == 'body') $this->findBody();
+			if ($findin == 'head') $this->findHead();
+			if ($findin == 'php' ) $this->findLayouts();
+			if ($findin == 'css' ) $this->findCSS();
+			if ($findin == 'js'  ) $this->findJS();
+			die(json_encode($r));
+		}
 
 	}
+	
+	private function findBody () {
 
+	}
+	
+	private function findHead () {
+
+	}
+	
+	private function findLayouts () {
+		foreach (glob("../layout.*.php") as $entry) {
+			// open and find in this file ... 
+			$content = file_get_contents($entry);
+			
+		}
+	}	
+	
+	private function findCSS () {
+		foreach (glob("../site-assets/css/*.css") as $entry) {
+			// open and find in this file ... 
+		}
+	}
+	
+	private function findJS () {
+		foreach (glob("../site-assets/js/*.js") as $entry) {
+			// open and find in this file ... 
+		}
+	}
+/*	
+	private function findinhead () {
+		$stmt = $this->prepare("SELECT id, pagename, url FROM `pages` WHERE 
+			`head` LIKE CONCAT ('%' , :findstr , '%') OR 
+			`title` LIKE CONCAT ('%' , :findstr , '%') OR 
+			`keywords` LIKE CONCAT ('%' , :findstr , '%') OR 
+			`description` LIKE CONCAT ('%' , :findstr , '%') ");
+		$stmt->bindParam(':findstr', $_POST['find'], PDO::PARAM_STR);
+		$stmt->execute();
+		$r = new stdClass();
+		$r->success = true;
+		$r->pages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		die(json_encode($r));		
+	}	
+	
+	private function findinbody($findstr) {
+		$stmt = $this->prepare("SELECT id, pagename, url FROM `pages` WHERE 
+			`maincontent` LIKE CONCAT ('%' , :findstr , '%') OR
+			`headercontent` LIKE CONCAT ('%' , :findstr , '%') OR
+			`footercontent` LIKE CONCAT ('%' , :findstr , '%') ");
+		$stmt->bindParam(':findstr', $_POST['find'], PDO::PARAM_STR);
+		$stmt->execute();
+		$r = new stdClass();
+		$r->success = true;
+		$r->pages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		die(json_encode($r));		
+	}
+	
+	private function replace($findstr, $replacestr) {
+		$r = new stdClass();
+		$r->success = true;
+		//$r->pages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		die(json_encode($r));		
+	}
+*/
 }
 ?>
--- login/find.php
+++ login/find.php
@@ -15,8 +15,12 @@ $cms = new ezFind();
 
 ?><!DOCTYPE html><html lang="en"><head>
 
-	<title>Global Find Replace : ezCMS Admin</title>
+	<title>Find Replace : ezCMS Admin</title>
 	<?php include('include/head.php'); ?>
+	<style>
+	textarea { height: auto; }
+	#frmreplace { display:none; }
+	</style>
 
 </head><body>
 
@@ -24,11 +28,61 @@ $cms = new ezFind();
 	<?php include('include/nav.php'); ?>
 	<div class="container">
 	  <div class="row-fluid">
-		<div class="span3 white-boxed">
-			This is the form .. 
+		<div class="span3">
+		
+		  <div id="frmfind" class="white-boxed"><form method="post" action="#">
+			<div class="navbar"><div class="navbar-inner">
+				<input type="submit" name="find" class="btn btn-primary pull-left" value="FIND">
+				<ul class="nav pull-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-flag"></i>
+							WHERE <b class="caret"></b></a>
+						<ul id="findinDD" class="dropdown-menu">
+							<li><a data-loc="body" href="#"><i class="icon-file"></i> Pages &gt;&gt; Body</a></li>
+							<li><a data-loc="head" href="#"><i class="icon-arrow-up"></i> Pages &gt;&gt; Head</a></li>
+							<li class="divider"></li>
+							<li class="nav-header">Template</li>
+							<li><a data-loc="php" href="#"><i class="icon-list-alt"></i> PHP Layouts</a></li>
+							<li><a data-loc="css" href="#"><i class="icon-pencil"></i> CSS Stylesheets</a></li>
+							<li><a data-loc="js" href="#"><i class="icon-align-left"></i> JS Javascripts</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div></div>
+
+			<div class="control-group">
+				<label class="control-label">FIND TEXT</label>
+				<div class="controls">
+					<textarea name="find" id="txtfind" rows="5"
+						placeholder="Enter the text or code to find"
+						class="input-block-level" required minlength="3"></textarea>
+				</div>
+			</div>
+			<input type="hidden" name="findinTxt" id="findinTxt" />
+		  </form></div><br>
+
+		  <div id="frmreplace" class="white-boxed"><form method="post" action="#">
+			<div class="navbar"><div class="navbar-inner">
+				<a id="repall" href="#" class="btn btn-danger">Replace</a>
+			</div></div>
+			<div class="control-group">
+				<label class="control-label">REPLACE WITH</label>
+				<div class="controls">
+					<textarea name="replace" id="txtreplace" rows="5"
+						placeholder="Enter the text or code to replace"
+						class="input-block-level"></textarea>
+				</div>
+			</div>			
+		  </form></div>
+			
 		</div>
 		<div class="span9 white-boxed">
-			this is the results of the search 
+			<div class="alert alert-info"><h4 id="findinlbl"></h4></div>
+			<table id="resultsTable" class="table table-striped"><thead>
+			<tr><th>Search Results</th></tr>
+			</thead><tbody>
+			<tr><td>No Results</td></tr>
+			</tbody></table>
 		</div>
 	  </div>
 	</div>
@@ -36,7 +90,48 @@ $cms = new ezFind();
 </div><!-- /wrap  -->
 
 <?php include('include/footer.php'); ?>
-<script type="text/javascript">
+<script>
+$('#findinDD a').click(function (e) {
+	// prevent defaults
+	e.preventDefaults;
+	$('#findinlbl').html('WHERE : ' + $(this).html());
+	$('#findinTxt').val($(this).data('loc'));
+	//return false;
+	
+}).eq(0).click();
+$('#frmfind form').submit(function (e) {
+	
+	// prevent defaults
+	e.preventDefaults;
+	
+	// ajax to the server
+	$.post( 'find.php?action=fetch', $( this ).serialize(), 
+		function(data) {
+			if (data.success) {
+				//alert('Done.');
+				var row;
+				$('#resultsTable tbody').empty();
+				$('#resultsTable thead tr').empty().html('<th>ID</th><th>NAME</th><th>URL</th><th>ACTION</th>');
+				for(var k in data.pages) {
+					row = '<td></td>';
+					row += '<td>'+data.pages[k].pagename+'</td>';
+					row += '<td>'+data.pages[k].url+'</td>';
+					row += '<td><a href="#">View</a> | <a href="#">Edit</a> | <a href="#">Replace</a></td>';
+					$('<tr></tr>').html(row).appendTo('#resultsTable tbody');
+				}
+				if ($('#resultsTable tbody').html() == '')
+					$('#resultsTable tbody').html('<td colspan="4">Nothing found for `'+$('#txtfind').val()+'`</td>');
+				
+			} else alert('Error: '+ data,msg);
+	}, 'json').fail( function() { 
+		alert('Failed: The request failed.'); 
+	});
+	
+	return false;
+	
+});
+</script>
+<script>
 	$("#top-bar li").removeClass('active');
 	$("#top-bar li:eq(10)").addClass('active');
 </script>
